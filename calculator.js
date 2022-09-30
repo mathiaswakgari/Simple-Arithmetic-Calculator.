17a7b313d5fa07698c968f319c5b692d8dfb43c3
Mathias Wakgari <104831041+mathiaswakgari@users.noreply.github.com>
2022-09-30 19:29:38 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -17,15 +17,39 @@ class Calculator{
     }
     selectOperation(operation){
         this.operation = operation
-        this.previousOperand = this.currentOperand;
+        this.previousOperand = this.currentOperand + this.operation;
         this.currentOperand = ''
     }
+    calculate(){
+      let result;
+      const previous = parseInt(this.previousOperand);
+      const current = parseInt(this.currentOperand);
+      switch (this.operation){
+          case '+':
+              result = previous + current
+              break
+          case '-':
+              result = previous - current
+              break
+          case '/':
+              result = previous / current
+              break
+          case '*':
+              result = previous * current
+              break
+          default:
+              return
+      }
+      this.currentOperand = result;
+      this.operation = undefined;
+      this.previousOperand = ''
 }
 
 const numberButtons = document.querySelectorAll('[data-number]');
 const previousOperandTextElement = document.querySelector('[data-previous]')
 const currentOperandTextElement = document.querySelector('[data-current]');
 const operationsButtons = document.querySelectorAll('[data-operands]');
+const equalsButton = document.querySelector('[data-equals]');
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
 
@@ -42,3 +66,7 @@ operationsButtons.forEach(button =>{
         calculator.updateDisplay();
     })
 })
+equalsButton.addEventListener('click', button=>{
+    calculator.calculate();
+    calculator.updateDisplay();
+} )
